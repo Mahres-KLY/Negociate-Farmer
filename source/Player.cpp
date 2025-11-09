401e80bcebad86fa68f39e9d4560867db2445a1c
Mahres <mraoelin@gmail.com>
2025-11-09 17:39:12 +0100

    Update Player.cpp

--- source/Player.cpp
+++ source/Player.cpp
@@ -8,7 +8,7 @@ Player::Player() {}
 
 bool Player::load(const string& path) {
     if (!texture.loadFromFile(path)) {
-        cerr << "Erreur: texture joueur non trouvée." << endl;
+        cerr << "Erreur: texture joueur non trouvÃ©e." << endl;
         return false;
     }
     sprite = make_unique<Sprite>(texture);
@@ -18,17 +18,17 @@ bool Player::load(const string& path) {
 
 void Player::handleInput() {
     Vector2f movement(0.f, 0.f);
-    if (Keyboard::isKeyPressed(Keyboard::Key::W)) movement.y -= 1.f;
+    if (Keyboard::isKeyPressed(Keyboard::Key::Z)) movement.y -= 1.f;
     if (Keyboard::isKeyPressed(Keyboard::Key::S)) movement.y += 1.f;
-    if (Keyboard::isKeyPressed(Keyboard::Key::A)) movement.x -= 1.f;
+    if (Keyboard::isKeyPressed(Keyboard::Key::Q)) movement.x -= 1.f;
     if (Keyboard::isKeyPressed(Keyboard::Key::D)) movement.x += 1.f;
 
     if (sprite)
-        sprite->move(movement * speed * 0.016f); // petit déplacement fixe pour test
+        sprite->move(movement * speed * 0.016f); // petit dÃ©placement fixe
 }
 
 void Player::update(float dt) {
-    // Ici tu pourras gérer les collisions ou animations plus tard
+    // les collisions ou animations 
 }
 
 void Player::render(RenderWindow& window) {
