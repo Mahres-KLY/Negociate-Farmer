49504eb8fd9c90d7bd3bc63dd055e82b819ada75
Mahres <mraoelin@gmail.com>
2025-11-09 18:10:22 +0100

    Update Gameplay.hpp

--- include/Gameplay.hpp
+++ include/Gameplay.hpp
@@ -1,21 +1,55 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "Player.hpp"
-#include "World.hpp"
-#include "Audio.hpp"
+#include <vector>
+#include <string>
 
-using namespace std;
 using namespace sf;
+using namespace std;
+
+class NPC {
+public:
+    RectangleShape shape;
+    string name;
+    bool isMainNPC;
+
+    NPC(Vector2f pos, string name, bool isMain = false) {
+        this->name = name;
+        this->isMainNPC = isMain;
+        shape.setSize({40, 60});
+        shape.setFillColor(isMain ? Color::Green : Color::Yellow);
+        shape.setPosition(pos);
+    }
+};
+
+class Player {
+public:
+    RectangleShape shape;
+
+    Player(Vector2f pos = {400, 500}) {
+        shape.setSize({40, 60});
+        shape.setFillColor(Color::Cyan);
+        shape.setPosition(pos);
+    }
+
+    void move(Vector2f dir) { shape.move(dir); }
+
+    FloatRect getBounds() const { return shape.getGlobalBounds(); }
+};
 
 class Gameplay {
 private:
-    RenderWindow window;
-    Player player;
-    World world;
-    Audio audio;
-    Clock clock; // Pour gérer le deltaTime
+    RenderWindow* m_window;
+    Player m_player;
+    vector<NPC> m_npcs;
+    Font m_font;
+    Text m_text;
+    bool inMiniGame = false;
+    bool inDialogue = false;
 
 public:
-    Gameplay();
+    Gameplay(RenderWindow* window);
     void run();
+    void handleInput();
+    void update();
+    void render();
 };
