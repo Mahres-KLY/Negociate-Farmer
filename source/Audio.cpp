d12015b2750223f237c8bd20b3a1ebf99aadd3e2
Mahres <mraoelin@gmail.com>
2025-11-09 17:30:36 +0100

    Update Audio.cpp

--- source/Audio.cpp
+++ source/Audio.cpp
@@ -4,9 +4,31 @@
 using namespace std;
 using namespace sf;
 
-void Audio::playMusic() {
+Audio::Audio() {
+}
+
+Audio::~Audio() {
+    music.stop();
+}
+
+bool Audio::loadMusic(const string& filePath) {
+    if (!music.openFromFile(filePath)) {
+        cerr << "Erreur : impossible de charger la musique : " << filePath << endl;
+        return false;
+    }
+    return true;
+}
+
+void Audio::playMusic(bool loop) {
+    music.play();
+    music.stop();
 }
 
 
-void Audio::stopMusic() {
+void Audio::stopMusic() { music.stop(); }
+
+void Audio::setVolume(float volume) {
+    if (volume < 0.f) volume = 0.f;
+    if (volume > 100.f) volume = 100.f;
+    music.setVolume(volume);
 }
