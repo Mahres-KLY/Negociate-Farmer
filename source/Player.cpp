09f1e4fae32ceb793cbd43c708fac9f48747b11e
Mahres <mraoelin@gmail.com>
2025-11-09 18:28:44 +0100

    Update Player.cpp

--- source/Player.cpp
+++ source/Player.cpp
@@ -1,37 +1,41 @@
 #include "Player.hpp"
-#include <iostream>
 
-using namespace std;
-using namespace sf;
-
-Player::Player() {}
-
-bool Player::load(const string& path) {
-    if (!texture.loadFromFile(path)) {
-        cerr << "Erreur: texture joueur non trouvée." << endl;
-        return false;
+Player::Player() {
+    if (!texture.loadFromFile("assets/player.png")) {
+        cout << "Erreur chargement texture du joueur !" << endl;
     }
-    sprite = make_unique<Sprite>(texture);
-    sprite->setPosition({ 640.f, 360.f });
-    return true;
+    sprite.setTexture(texture);
+    sprite.setPosition(400, 300);
+    speed = 200.f;
 }
 
-void Player::handleInput() {
+void Player::handleInput(float deltaTime) {
     Vector2f movement(0.f, 0.f);
-    if (Keyboard::isKeyPressed(Keyboard::Key::Z)) movement.y -= 1.f;
-    if (Keyboard::isKeyPressed(Keyboard::Key::S)) movement.y += 1.f;
-    if (Keyboard::isKeyPressed(Keyboard::Key::Q)) movement.x -= 1.f;
-    if (Keyboard::isKeyPressed(Keyboard::Key::D)) movement.x += 1.f;
 
-    if (sprite)
-        sprite->move(movement * speed * 0.016f); // petit déplacement fixe
+    if (Keyboard::isKeyPressed(Keyboard::Z)) movement.y -= speed * deltaTime;
+    if (Keyboard::isKeyPressed(Keyboard::S)) movement.y += speed * deltaTime;
+    if (Keyboard::isKeyPressed(Keyboard::Q)) movement.x -= speed * deltaTime;
+    if (Keyboard::isKeyPressed(Keyboard::D)) movement.x += speed * deltaTime;
+
+    sprite.move(movement);
 }
 
-void Player::update(float dt) {
-    // les collisions ou animations 
+void Player::update(float deltaTime) {
+    handleInput(deltaTime);
 }
 
-void Player::render(RenderWindow& window) {
-    if (sprite)
-        window.draw(*sprite);
+void Player::draw(RenderWindow& window) {
+    window.draw(sprite);
+}
+
+void Player::move(Vector2f dir) {
+    sprite.move(dir);
+}
+
+Vector2f Player::getPosition() const {
+    return sprite.getPosition();
+}
+
+FloatRect Player::getBounds() const {
+    return sprite.getGlobalBounds();
 }
