0a41b12578fb3d511351d4f77348b5a3c5fb0434
Mahres <mraoelin@gmail.com>
2025-11-09 17:42:35 +0100

    Update MapLoader.hpp

--- include/MapLoader.hpp
+++ include/MapLoader.hpp
@@ -3,16 +3,19 @@
 #include <tmxlite/Map.hpp>
 #include <tmxlite/Layer.hpp>
 #include <tmxlite/TileLayer.hpp>
+#include <vector>
+#include <string>
+#include <iostream>
 
 using namespace std;
 using namespace sf;
 
 class MapLoader : public Drawable {
 private:
+    vector<Texture> m_tilesets;
     VertexArray m_vertices;
-    Texture m_tileset;
 
 public:
     bool load(const string& tmxFile, const string& tilesetPath);
-    virtual void draw(RenderTarget& target, RenderStates states) const override;
+    void draw(RenderTarget& target, RenderStates states) const override;
 };
