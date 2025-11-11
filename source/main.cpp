d981af5bea9e3b5beddf0894b5183ee71f5375ca
Mahres <mraoelin@gmail.com>
2025-11-11 23:40:08 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -2,15 +2,22 @@
 #include "Gameplay.hpp"
 #include <iostream>
 
-int main() {
-    RenderWindow window(VideoMode({ 800, 600 }), "Map Test");
+using namespace sf;
 
+int main() {
     bool startGame = false;
+
+    // Étape 1 : Menu de démarrage
     Start startMenu;
     startMenu.run(startGame);
 
+    // Étape 2 : Si le joueur a cliqué sur PLAY, on lance le jeu
     if (startGame) {
-        Gameplay gameplay;
+        // Création de la fenêtre de jeu (réutilisation des dimensions du menu)
+        RenderWindow gameWindow(VideoMode({ 1280, 720 }), "Territory Farm - Gameplay");
+
+        // Lancement du gameplay avec la fenêtre
+        Gameplay gameplay(&gameWindow);
         gameplay.run();
     }
 
